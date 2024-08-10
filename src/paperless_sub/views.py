b305e07e45e4f704f5143dd51d2eed3d2557879e
sublimz <sublimz@gmail.com>
2024-08-10 21:40:43 +0200

    try another method

--- src/paperless_sub/views.py
+++ src/paperless_sub/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.models import User
 from documents.views import *
 
 
@@ -28,29 +27,20 @@ class PublicIndexView(TemplateView):
             return lang
 
     def get_context_data(self, **kwargs):
-
-        if request.user.is_anonymous:
-            # Try to get or create the user
-            user, created = User.objects.get_or_create(
-                username=request.session.session_key,
-                defaults=User.objects.filter(username='public')
-            )
-            login(request,user)
-            context = super().get_context_data(**kwargs)
-            context["cookie_prefix"] = settings.COOKIE_PREFIX
-            context["username"] = self.request.user.username
-            context["full_name"] = self.request.user.get_full_name()
-            context["styles_css"] = f"publicfrontend/{self.get_frontend_language()}/styles.css"
-            context["runtime_js"] = f"publicfrontend/{self.get_frontend_language()}/runtime.js"
-            context["polyfills_js"] = (
-                f"publicfrontend/{self.get_frontend_language()}/polyfills.js"
-            )
-            context["main_js"] = f"publicfrontend/{self.get_frontend_language()}/main.js"
-            context["webmanifest"] = (
-                f"publicfrontend/{self.get_frontend_language()}/manifest.webmanifest"
-            )
-            context["apple_touch_icon"] = (
-                f"publicfrontend/{self.get_frontend_language()}/apple-touch-icon.png"
-            )
-
+        context = super().get_context_data(**kwargs)
+        context["cookie_prefix"] = settings.COOKIE_PREFIX
+        context["username"] = self.request.user.username
+        context["full_name"] = self.request.user.get_full_name()
+        context["styles_css"] = f"publicfrontend/{self.get_frontend_language()}/styles.css"
+        context["runtime_js"] = f"publicfrontend/{self.get_frontend_language()}/runtime.js"
+        context["polyfills_js"] = (
+            f"publicfrontend/{self.get_frontend_language()}/polyfills.js"
+        )
+        context["main_js"] = f"publicfrontend/{self.get_frontend_language()}/main.js"
+        context["webmanifest"] = (
+            f"publicfrontend/{self.get_frontend_language()}/manifest.webmanifest"
+        )
+        context["apple_touch_icon"] = (
+            f"publicfrontend/{self.get_frontend_language()}/apple-touch-icon.png"
+        )
         return context
