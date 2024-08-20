3ab89f699c189f8bc6e9447c6473c9907e5c2337
sublimz <sublimz@gmail.com>
2024-08-20 08:37:27 +0200

    initialization doc dataform

--- src/paperless_sub/apps.py
+++ src/paperless_sub/apps.py
@@ -8,58 +8,75 @@ class PaperlessSubConfig(AppConfig):
 
         from paperless_sub import signals
 
-        from django.dispatch import receiver
-        from django.db import DatabaseError, OperationalError
-        from django.contrib.auth.models import User, Permission
-        from django.contrib.contenttypes.models import ContentType
-        from documents.models import User as DocumentUser
-        from documents.models import CustomField
-        from documents.models import Workflow
-        from documents.models import WorkflowAction
-        from documents.models import WorkflowTrigger
 
-        import logging
-
-        try:
-            if not DocumentUser.objects.filter(username='consumer_anonyme').exists():
-                # Créer les utilisateurs système
-                add_document_permission=Permission.objects.get(codename='add_document', content_type__app_label='documents')
-                system_user_1 = DocumentUser.objects.create_user(
-                    username='consumer_anonyme',
-                    first_name='consumer_anonyme',
-                    last_name='consumer_anonyme',
-                    password='consumer_anonyme_sub',
-                    is_active=True
-                )
-                system_user_1.user_permissions.add(add_document_permission)
-
-            if not DocumentUser.objects.filter(username='public').exists():
-                view_uisettings_permission = Permission.objects.get(codename='view_uisettings', content_type__app_label='documents')
-                view_document_permission = Permission.objects.get(codename='view_document', content_type__app_label='documents')  
-                system_user_2 = DocumentUser.objects.create_user(
-                    username='public',
-                    first_name='public',
-                    last_name='public',
-                    password='public',
-                    is_active=True
-                )
-                system_user_2.user_permissions.add(view_uisettings_permission, view_document_permission)
-
-            if not CustomField.objects.filter(name='Date de début de publication').exists():
-                CustomField.objects.create(name='Date de début de publication',data_type='date')
-
-            if not CustomField.objects.filter(name='Date de fin de publication').exists():
-                CustomField.objects.create(name='Date de fin de publication',data_type='date')    
-
-            """
-            if not Workflow.objects.filter(name='[SUB] ajoute champ perso date debut pub et date fin pub').exists()
-                Workflow.objects.create(name='[SUB] ajoute champ perso date debut pub et date fin pub',order=1,enabled=1)
-                WorkflowAction.objects.create(workflow_id=1,workflowaction_id=1)
-                WorkflowTrigger.objects.create(workflow_id=1,workflowtrigger_id=1)
-            """
-
-        except:
-            pass
+#        from django.dispatch import receiver
+#        from django.db import DatabaseError, OperationalError
+#        from django.contrib.auth.models import User, Permission, Group
+#        from django.contrib.contenttypes.models import ContentType
+#        from documents.models import User as DocumentUser
+#        from documents.models import CustomField
+#        from documents.models import Workflow
+#        from documents.models import WorkflowAction
+#        from documents.models import WorkflowTrigger
+#
+#        import logging
+#
+#        try:
+#            if not DocumentUser.objects.filter(username='consumer_anonyme').exists():
+#                # Créer les utilisateurs système
+#                add_document_permission=Permission.objects.get(codename='add_document', content_type__app_label='documents')
+#                system_user_1 = DocumentUser.objects.create_user(
+#                    username='consumer_anonyme',
+#                    first_name='consumer_anonyme',
+#                    last_name='consumer_anonyme',
+#                    password='consumer_anonyme_sub',
+#                    is_active=True
+#                )
+#                system_user_1.user_permissions.add(add_document_permission)
+#
+#            if not DocumentUser.objects.filter(username='public').exists():
+#                view_uisettings_permission = Permission.objects.get(codename='view_uisettings', content_type__app_label='documents')
+#                view_document_permission = Permission.objects.get(codename='view_document', content_type__app_label='documents')  
+#                system_user_2 = DocumentUser.objects.create_user(
+#                    username='public',
+#                    first_name='public',
+#                    last_name='public',
+#                    password='public',
+#                    is_active=True
+#                )
+#                system_user_2.user_permissions.add(view_uisettings_permission, view_document_permission)
+#
+#            if not Group.objects.filter(name='instructeur').exists():
+#                add_document_permission=Permission.objects.get(codename='add_document', content_type__app_label='documents')
+#                view_uisettings_permission = Permission.objects.get(codename='view_uisettings', content_type__app_label='documents')
+#                view_document_permission = Permission.objects.get(codename='view_document', content_type__app_label='documents')  
+#                instructeur_group, created = Group.objects.get_or_create(name='instructeur')
+#                instructeur_group.permissions.add(add_document_permission,view_uisettings_permission, view_document_permission)
+#
+#            if not Group.objects.filter(name='public').exists():
+#                view_uisettings_permission = Permission.objects.get(codename='view_uisettings', content_type__app_label='documents')
+#                view_document_permission = Permission.objects.get(codename='view_document', content_type__app_label='documents')  
+#                public_group, created = Group.objects.get_or_create(name='public')
+#                public_group.permissions.add(view_uisettings_permission, view_document_permission)    
+#
+#            if not CustomField.objects.filter(name='Date de début de publication').exists():
+#                CustomField.objects.get_or_create(name='Date de début de publication',data_type='date')
+#
+#            if not CustomField.objects.filter(name='Date de fin de publication').exists():
+#                CustomField.objects.get_or_create(name='Date de fin de publication',data_type='date')
+#
+#            if not CustomField.objects.filter(name='A publier').exists():
+#                CustomField.objects.get_or_create(name='A publier',data_type='boolean')            
+#
+#            
+#            if not Workflow.objects.filter(name='[SUB] ajoute champ perso date debut pub et date fin pub').exists():
+#                Workflow.objects.get_or_create(name='[SUB] ajoute champ perso date debut pub et date fin pub',order=1,enabled=1)
+#                WorkflowAction.objects.get_or_create(workflow_id=1,workflowaction_id=1)
+#                WorkflowTrigger.objects.get_or_create(workflow_id=1,workflowtrigger_id=1)
+#
+#
+#        except:
+#            pass
 
 
         AppConfig.ready(self)
--- src/paperless_sub/signals.py
+++ src/paperless_sub/signals.py
@@ -7,35 +7,90 @@ from django.db.models.signals import pre_save, post_save
 from documents.models import Document
 from documents.models import CustomField
 from documents.models import CustomFieldInstance
+from documents.tasks import consume_file
 from celery import shared_task
 from celery.signals import task_postrun
-
+from auditlog.models import LogEntry
 
 logger = logging.getLogger("paperless.handlers")
 
+#Après ajout du doc initialisation des valeurs
 @task_postrun.connect
-def task_postrun_handler(sender=None, **kwargs):
-    print(f"-------------------  Tâche terminée : --{sender.name}- result: {kwargs['retval']}")
-
-    if sender.name == "documents.tasks.consume_file" :
-        pattern = r'id (\d+)'
-        match = re.search(pattern, kwargs['retval'])
+def task_postrun_handler(sender=consume_file, **kwargs):
+    print(f"-------------------  Tâche terminée : --{sender.name}- result:--{kwargs['retval']}--")
+    match = re.search(r"(?<=Success\. New document id )\d+(?= created)", str(kwargs['retval']))
         
-        if match:
-            doc_id = match.group(1)
-            dp=CustomField.objects.get(name='Date de début de publication')
-            ddp=CustomFieldInstance.objects.get(document_id=doc_id,field_id=dp.id)
-            fp=CustomField.objects.get(name='Date de fin de publication')
-            dfp=CustomFieldInstance.objects.get(document_id=doc_id,field_id=fp.id)
+    if match:
+        doc_id = int(match.group())
+        print(f"{doc_id} de type {type(doc_id)}")
+        #date de début de publication à la date du jour
+        dp=CustomField.objects.get(name='Date de début de publication')
+        ddp=CustomFieldInstance.objects.get(document_id=doc_id,field_id=dp.id)
+        if ddp.value_date is None:
+            ddp.value_date=date.today()
+            ddp.save()
+        #date de fin de publication dans 60 jrs
+        fp=CustomField.objects.get(name='Date de fin de publication')
+        dfp=CustomFieldInstance.objects.get(document_id=doc_id,field_id=fp.id)
+        if dfp.value_date is None:
+            dfp.value_date=date.today() + timedelta(days=60)
+            dfp.save()
+        #Publier à faux
+        p=CustomField.objects.get(name='Publier')
+        cp=CustomFieldInstance.objects.get(document_id=doc_id,field_id=p.id)
+        if cp.value_bool is None:
+            cp.value_bool=False
+            cp.save()
 
-            if ddp.value_date is None:
-                ddp.value_date=date.today()
-                ddp.save()
-            if dfp.value_date is None:
-                dfp.value_date=date.today() + timedelta(days=60)
-                dfp.save()
 
-@receiver(post_save, sender=Document)
-def custom_field_instance_updated(sender, instance, created, **kwargs):
+            
+#Evaluation de si on publie
+@receiver(post_save, sender=CustomFieldInstance)
+def custom_fields_post_save(sender, instance, created, **kwargs):
     if not created:
-        print(f"L'objet {instance} a été mis à jour.")
+        id_cf_publier=CustomField.objects.get(name='Publier')
+        #si la màj concerne le champ Publier et que sa valeur est True 
+        if instance.field_id==id_cf_publier.id and instance.value_bool==True:
+            #print(f"{id_cf_publier.id}L'objet CustomField {instance.id} et le champ {instance.field_id} à pris la valeur {instance.value_bool} pour le {instance.document_id} par {sender}")
+            try:
+                print(f"on publie le {instance.document_id}")
+                
+            except:
+                pass
+                #CustomFieldInstance.objects.get(document_id=1,field_id=3).delete()
+
+#note = Note.objects.get(id=int(request.GET.get("id")))
+#if settings.AUDIT_LOG_ENABLED:
+#    LogEntry.objects.log_create(
+#        instance=doc,
+#        changes=json.dumps(
+#            {
+#                "Note Deleted": [note.id, "None"],
+#            },
+#        ),
+#        action=LogEntry.Action.UPDATE,
+#    )
+
+#    doc=instance
+#    if created:
+#        print(f"L'objet {doc.id} a été créé par {sender}")
+#
+#
+#    if not created:
+#        print(f"L'objet {doc.id} a été mis à jour par {sender}")
+#
+#	    ##Détection de à publier
+#        qs=CustomFieldInstance.objects.get(document_id=doc.id, field_id=CustomField.objects.get(name='Publier'))
+#        print(f"qs à la valeur {qs.value_bool}")
+#        ##Retourne faux si la checkbox est coché
+#        print(qs.value_bool)
+#
+#        if qs.value_bool == False :
+#            print("on veut publier")
+#            print(qs.id)
+#            entries = LogEntry.objects.filter(object_pk=qs.id,object_repr="Publier : False").order_by('-timestamp').values_list('id', flat=True)
+#            eid_list = list(entries)
+#            print(eid_list)
+#            if eid_list is None:
+#                print("----- jamais publier, on va publier")
+#
