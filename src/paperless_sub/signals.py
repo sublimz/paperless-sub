a85e46247eeff2724ecb125d41e16952a3c58894
sublimz <sublimz@gmail.com>
2024-09-08 10:57:54 +0200

    add tag system workflow #1

--- src/paperless_sub/signals.py
+++ src/paperless_sub/signals.py
@@ -7,6 +7,7 @@ from django.dispatch import receiver
 import logging
 from django.db.models.signals import pre_save, post_save
 from documents.models import Document
+from documents.models import Tag
 from documents.models import CustomField
 from documents.models import CustomFieldInstance
 from documents.tasks import consume_file,bulk_update_documents,update_document_archive_file
@@ -53,12 +54,99 @@ def task_postrun_handler(sender=consume_file, **kwargs):
             cp.value_bool=False
             cp.save()
 
+
+"""
+@receiver(m2m_changed, sender=Document.tags.through)
+def handle_tags_change(sender, **kwargs):
+    print(f"tags_changed {sender} {kwargs['action']}")
+    if kwargs['action']=="pre_add":
+        instance = kwargs['instance']  # Récupérer l'instance
+        document_id = instance.id      # Accéder à l'ID de l'objet Document
+        print(document_id)
+        doc=Document.objects.get(id=document_id)
+        print(f"Document tags {doc.tags.all()}") 
+"""
+
+@receiver(m2m_changed, sender=Document.tags.through)
+def handle_tags_change(sender, instance, action, **kwargs):
+    if action == "pre_add":
+        # Récupérer les IDs des objets Tag qui vont être ajoutés
+        new_tag_ids = kwargs.get('pk_set', set())
+        
+        # Récupérer les noms des tags qui vont être ajoutés
+        new_tags = Tag.objects.filter(id__in=new_tag_ids).values_list('name', flat=True)
+        
+        # Récupérer les noms des tags déjà associés
+        current_tag_ids = instance.tags.values_list('id', flat=True)
+        current_tags = Tag.objects.filter(id__in=current_tag_ids).values_list('name', flat=True)
+        
+        print("Tags à ajouter :", list(new_tags))
+        print("Tags déjà associés :", list(current_tags))
+
+        if None in current_tags:
+            print("instance.notag()")
+
+        # Vérifier les conditions
+        # Si à instruire vers en ligne
+        if "A instruire" in current_tags and "En ligne" in new_tags:
+            if "Archive" not in current_tags and "Archive" not in new_tags:
+                print("instance.stamp()")  # Appeler la méthode stamp()
+                tag_a_instruire = Tag.objects.get(name="A instruire")
+                tag_nouveau = Tag.objects.get(name="Nouveau")
+                instance.tags.remove(tag_a_instruire)
+                instance.tags.remove(tag_nouveau)
+                
+
+
+
+## Exemple d'utilisation
+#object_id = 1  # Remplacez par l'ID de l'objet que vous souhaitez vérifier
+#search_value = "Add Tags: En ligne"
+#
+#if log_entry_exists(YourModel, object_id, search_value):
+#    print("L'entrée de log existe.")
+#else:
+#    print("Aucune entrée de log trouvée.")
+#
+#
+#
+#def log_entry_exists(model, object_id, search_value):
+#    """
+#    Vérifie si une entrée de log existe pour un objet donné et une valeur spécifiée.
+#
+#    :param model: Le modèle de l'objet
+#    :param object_id: L'ID de l'objet
+#    :param search_value: La valeur à rechercher dans le message de changement
+#    :return: True si l'entrée de log existe, sinon False
+#    """
+#    # Obtenez le ContentType de votre modèle
+#    content_type = ContentType.objects.get_for_model(model)
+#
+#    # Rechercher les LogEntry pour cet objet
+#    log_entries = LogEntry.objects.filter(
+#        content_type=content_type,
+#        object_id=object_id,
+#        action_flag=ADDITION,
+#        change_message__icontains=search_value
+#    )
+#
+#    # Retourne True si des entrées sont trouvées, sinon False
+#    return log_entries.exists()
+
+
+
+
 @receiver(document_updated)
 def mon_recepteur(sender, **kwargs):
     doc = kwargs.get('document')
 
-    # Vérification et status du champ publier
+    # Vérification et statut du champ publier
     id_cf_publier=CustomField.objects.get(name='Publier')
+    
+    # On récupère la liste des tags
+    current_tag_ids = doc.tags.values_list('id', flat=True)
+    current_tags = Tag.objects.filter(id__in=current_tag_ids).values_list('name', flat=True)
+
     if CustomFieldInstance.objects.filter(document_id=doc.id,field_id=id_cf_publier.id).exists():
         cf_doc=CustomFieldInstance.objects.get(document_id=doc.id,field_id=id_cf_publier.id)
         print(f"Document mis à jour : {doc.id} {doc.title} {doc.archive_path}")
@@ -67,7 +155,7 @@ def mon_recepteur(sender, **kwargs):
         if cf_doc.value_bool == True:    
             print(f"traitement de la publication pour {doc.id} {doc.archive_path}")
             try:
-                if check_dates_conformity(doc.id) and check_doc_type_conformity(doc.id):
+                if check_dates_conformity(doc.id) and check_doc_type_conformity(doc.id) and "Archive" not in current_tags and "En ligne" not in current_tags :
                     mySignTest=SignDocument()
                     doc.checksum = hashlib.md5(doc.archive_path.read_bytes()).hexdigest()
 
@@ -81,15 +169,20 @@ def mon_recepteur(sender, **kwargs):
                         print(f"tampon ajouté sur {doc.id} {doc.archive_path}")
                         
                         #Document.objects.filter(id=doc.id).update(modified=now())
-
                         #Suppression du champ publier
                         cf_doc.delete()
                         
                         #On ajoute le flag en ligne
-                        id_cf_online, created=CustomField.objects.get_or_create(name='En ligne',data_type="boolean")
-                        cf_online, created=CustomFieldInstance.objects.get_or_create(document_id=doc.id,field_id=id_cf_online.id)
-                        cf_online.value_bool=True
-                        cf_online.save()
+                        tag_en_ligne = Tag.objects.get(name="En ligne")
+                        #tag_a_instruire = Tag.objects.get(name="A instruire")
+                        #tag_archive = Tag.objects.get(name="Archive")
+                        doc.tags.add(tag_en_ligne)
+
+                        #Ajout du custom flag en ligne
+                        #id_cf_online, created=CustomField.objects.get_or_create(name='En ligne',data_type="boolean")
+                        #cf_online, created=CustomFieldInstance.objects.get_or_create(document_id=doc.id,field_id=id_cf_online.id)
+                        #cf_online.value_bool=True
+                        #cf_online.save()
 
                         #Changement des permissions
                         g_public, created = Group.objects.get_or_create(name='public')
